867da234dc187426cc058d099f8322d0f2b1ee1e
makeit1 <5378884+makeit1@users.noreply.github.com>
2019-05-12 21:17:46 +0900

    issue-5: changed to create a working directory in new session

---
+++ Rapid Reporter/CustomException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Rapid_Reporter
+{
+    public class InvalidDirecotoryException : Exception
+    {
+        public InvalidDirecotoryException() : base() { }
+        public InvalidDirecotoryException(string message) : base(message) { }
+        public InvalidDirecotoryException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
